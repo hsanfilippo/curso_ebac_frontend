05ee01f1b4d7fcf03c88c1701c446d9d1c651bd5
Henrique Sanfilippo <135433542+hsanfilippo@users.noreply.github.com>
2023-10-27 23:27:19 +0900

    Correção do exercício

--- array_objetos.js
+++ array_objetos.js
@@ -1,37 +1,52 @@
-class Alunos {
-    constructor(id, nome, nota) {
-        this.Id = id;
-        this.Nome = nome;
-        this.Nota = nota;
-    }
+function gerarNota() {
+    const nota = Math.floor(Math.random() * 11);
+    return nota; 
 }
 
-const joao = new Alunos(123, 'João', 7);
-const paula = new Alunos(124, 'Paula', 6);
-const maria = new Alunos(125, 'Maria', 5);
-const ana = new Alunos(126, 'Ana', 8);
-
-let alunos = [joao, paula, maria, ana]
-
-function confereAprovados() {
-    console.log('\nLista de alunos aprovados (nota de corte: 6)');
-    for (i = 0; i < alunos.length; i++) {
-        const valueNotas = alunos[i].Nota;
-        if (valueNotas >= 6) {
-            console.log(`${alunos[i].Nome} tirou ${alunos[i].Nota}`);
-        }
-    }
+function mostraAprovados() {
+    console.log('\n\n--- Listagem de alunos aprovados --- \n');
+    console.log(alunosAprovados, '\n');
+    console.log('--- Alunos em Recuperação --- \n');
+    console.log(alunosRecuperacao, '\n');
+    console.log('--- Fim --- \n');
 }
 
-function confereReprovados() {
-    console.log('\nLista de alunos reprovados (nota de corte: 6)');
-    for (i = 0; i < alunos.length; i++) {
-        const valueNotas = alunos[i].Nota;
-        if (valueNotas <= 5) {
-            console.log(`${alunos[i].Nome} tirou ${alunos[i].Nota}`);
-        }
+const alunos = [
+    {
+        Nome: "João",
+        Nota: gerarNota()
+    },
+    {
+        Nome: "Maria",
+        Nota: gerarNota()
+    },
+    {
+        Nome: "Zézinho",
+        Nota: gerarNota()
+    },
+    {
+        Nome: "Ana",
+        Nota: gerarNota()
+    },
+    {
+        Nome: "Julia",
+        Nota: gerarNota()
+    },
+    {
+        Nome: "Rubão",
+        Nota: gerarNota()
+    },
+    {
+        Nome: "Pablo",
+        Nota: gerarNota()
+    },
+    {
+        Nome: "Henrique",
+        Nota: gerarNota()
     }
-}
+]
 
-confereAprovados();
-confereReprovados();
+const alunosAprovados = alunos.filter(alunos => alunos.Nota >= 6);
+const alunosRecuperacao = alunos.filter(alunos => alunos.Nota < 6);
+
+mostraAprovados()
