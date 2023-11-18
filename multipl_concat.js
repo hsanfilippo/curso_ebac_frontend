29cdea0478738eca5c733f6a9b7a6f0d9a7b6ab4
Henrique Sanfilippo <135433542+hsanfilippo@users.noreply.github.com>
2023-11-18 20:22:32 +0900

    Add files via upload

---
+++ multipl_concat.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função simples de multiplicação com TypeScript:
+function multiplicaType(x, y) {
+    return x * y;
+}
+// Função simples de concatenação com TypeScript:
+function concatenaNome(nome) {
+    return `Olá ${nome}, seja bem vindo!`;
+}
+// Testando os retornos das funções:
+console.log(multiplicaType(120, 2));
+console.log(concatenaNome('Henrique'));
---
+++ multipl_concat.ts
@@ -0,0 +1,14 @@
+// Função simples de multiplicação com TypeScript:
+function multiplicaType(x: number, y: number): number {
+    return x * y;
+}
+
+
+// Função simples de concatenação com TypeScript:
+function concatenaNome(nome: string): string {
+    return `Olá ${nome}, seja bem vindo!`;
+}
+
+// Testando os retornos das funções:
+console.log(multiplicaType(120, 2));
+console.log(concatenaNome('Henrique'));
